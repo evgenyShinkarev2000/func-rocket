bdde7eeb5b9087028accfd05696b6ddafdff42a1
Evgeny.Shinkarev@urfu.me <Evgeny.Shinkarev@urfu.me>
2021-03-21 12:45:05 +0500

    after 2 review

--- ForcesTask.cs
+++ ForcesTask.cs
@@ -10,21 +10,23 @@ namespace func_rocket
 		/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
 		/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
 		/// </summary>
-		public static RocketForce GetThrustForce(double forceValue)
-			=> r => new Vector(forceValue, 0).Rotate(r.Direction);
+		public static RocketForce GetThrustForce(double forceValue) => 
+			rocket => new Vector(forceValue, 0).Rotate(rocket.Direction);
 
 			/// <summary>
 		/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
 		/// </summary>
-		public static RocketForce ConvertGravityToForce(Gravity gravity, Size spaceSize)
-			=> r => gravity(spaceSize, r.Location);
+		public static RocketForce ConvertGravityToForce(Gravity gravity, Size spaceSize) => 
+				rocket => gravity(spaceSize, rocket.Location);
 
 			 /// <summary>
 		/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
 		/// </summary>
-		public static RocketForce Sum(params RocketForce[] forces)
-				 => r => forces.Length == 0 
+		public static RocketForce Sum(params RocketForce[] forces) => 
+				 rocket => forces.Length == 0 
 					 ? Vector.Zero
-					 : forces.Select(x => x(r)).Aggregate((x, y) => x + y);
+					 : forces
+						 .Select(force => force(rocket))
+						 .Aggregate((forceVectorBegin, forceVectorOther) => forceVectorBegin + forceVectorOther);
 	}
 }
